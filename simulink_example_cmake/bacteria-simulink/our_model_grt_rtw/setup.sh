af44e35ded14b63aaa97617687a320fcc980845e
haliliceylan <mrhaliliceylan@gmail.com>
2022-05-11 09:40:44 +0000

    enable lcov

--- simulink_example_cmake/bacteria-simulink/our_model_grt_rtw/setup.sh
+++ simulink_example_cmake/bacteria-simulink/our_model_grt_rtw/setup.sh
@@ -2,10 +2,5 @@
 
 mkdir -p build
 pushd build
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGTEST_DIR=../../vendor/fused_gmock -LH
-popd
-
-mkdir -p build_cov
-pushd build_cov
-cmake .. -DGTEST_DIR=../../vendor/fused_gmock -DGCOV=ON -LH
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGTEST_DIR=../../vendor/fused_gmock -DGCOV=ON -LH
 popd
