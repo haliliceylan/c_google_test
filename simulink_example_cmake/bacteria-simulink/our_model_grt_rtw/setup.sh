048a66d3ed688de5c1b61b8fdef07271201ece98
haliliceylan <mrhaliliceylan@gmail.com>
2022-05-11 09:05:40 +0000

    cmake compability for jenkins

--- simulink_example_cmake/bacteria-simulink/our_model_grt_rtw/setup.sh
+++ simulink_example_cmake/bacteria-simulink/our_model_grt_rtw/setup.sh
@@ -2,10 +2,10 @@
 
 mkdir -p build
 pushd build
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGTEST_DIR=../../vendor/fused_gmock
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGTEST_DIR=../../vendor/fused_gmock -LH
 popd
 
 mkdir -p build_cov
 pushd build_cov
-cmake .. -DGTEST_DIR=../../vendor/fused_gmock -DGCOV=ON
+cmake .. -DGTEST_DIR=../../vendor/fused_gmock -DGCOV=ON -LH
 popd
